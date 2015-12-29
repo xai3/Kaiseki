77b6893cbabced6223e65abe1efc08aea511bd09
yukiasai <yukiasai@gmail.com>
2015-12-29 09:06:11 +0900

    Write tests for toJSON case of basic type is nil.

--- MoulinTests/ToJSONTests.swift
+++ MoulinTests/ToJSONTests.swift
@@ -25,17 +25,25 @@ class ToJSONTests: XCTestCase {
         let object = Object()
         object.boolTrue.value = true
         object.boolFalse.value = false
+        object.boolNull.value = nil
         object.int.value = 1
+        object.intNull.value = nil
         object.float.value = 10
+        object.floatNull.value = nil
         object.double.value = 100
+        object.doubleNull.value = nil
         
         let json = object.toJSON() as? [String: AnyObject]
         XCTAssertNotNil(json)
         XCTAssertEqual(json!["boolTrue"] as? Bool, true)
         XCTAssertEqual(json!["boolFalse"] as? Bool, false)
+        XCTAssertNil(json!["boolNull"])
         XCTAssertEqual(json!["int"] as? Int, 1)
+        XCTAssertNil(json!["intNull"])
         XCTAssertEqual(json!["float"] as? Float, 10)
+        XCTAssertNil(json!["floatNull"])
         XCTAssertEqual(json!["double"] as? Double, 100)
+        XCTAssertNil(json!["doubleNull"])
     }
     
     func testArray() {
